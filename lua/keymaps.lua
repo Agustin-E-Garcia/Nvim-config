b193f85d54d414863eb66bf269cf5ce0600f6a01
Agustin-E-Garcia <Agustinezequielprogrammer@gmail.com>
2025-08-01 20:05:47 -0300

    Added more options and keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,8 +1,30 @@
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
--- Buffer list
-map('n', '<leader><leader>', '<cmd>Telescope buffers<cr>', opts)
+map('n', '<Esc>', '<cmd>nohlsearch<cr>')
 
--- Git files
+map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uitckfix list' })
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Higlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('highlight-yank', { clear = true}),
+    callback = function()
+        vim.hl.on_yank()
+    end,
+})
+
+-- Search files
+map('n', '<leader><leader>', '<cmd>Telescope buffers<cr>', opts)
 map('n', '<leader>sg', '<cmd>Telescope git_files<cr>', opts)
+map('n', '<leader>sd', '<cmd>Telescope diagnostics<cr>', opts)
+
+-- LSP related on attach
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+    callback = function(event)
+        map('n', '<leader>rn', vim.lsp.buf.rename)
+        map('n', '<leader>gr', '<cmd>Telescope lsp_references<cr>')
+        map('n', '<leader>gd', '<cmd>Telescope lsp_definitions<cr>')
+        map('n', '<leader>gi', '<cmd>Telescope lsp_implementations<cr>')
+    end,
+})
--- lua/options.lua
+++ lua/options.lua
@@ -1,7 +1,24 @@
 vim.opt.number = true
+vim.g.have_nerd_font = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.termguicolors = true
 vim.opt.mouse = 'a'
+
+vim.schedule(function()
+    vim.o.clipboard = 'unnamedplus'
+end)
+
+vim.o.breakindent = true
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+vim.o.list = true
+vim.opt.listchars = { tab = '> ', trail = '-', nbsp = '_' }
+
+vim.o.scrolloff = 10

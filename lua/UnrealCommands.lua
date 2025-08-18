d6146f529344e2f38ba94f5a3d20924a7220ff59
Agustin-E-Garcia <Agustinezequielprogrammer@gmail.com>
2025-08-18 14:29:35 -0300

    UECompile now copies compile_commands file to uproject directory

--- lua/UnrealCommands.lua
+++ lua/UnrealCommands.lua
@@ -1,61 +1,12 @@
-vim.api.nvim_create_user_command('CreateUproject', function(opts)
-    local version = opts.args or "5.6"
-    local lines = {
-        '{',
-        '    "FileVersion": 3,',
-        '    "EngineAssociation": "'..version..'",',
-        '    "Category": "",',
-        '    "Description": "",',
-        '    "Modules": [',
-        '        {',
-        '            "Name": "",',
-        '            "Type": "Runtime",',
-        '            "LoadingPhase": "Default"',
-        '        }',
-        '    ]',
-        '}',
-  }
-  vim.api.nvim_put(lines, 'l', true, true)
-end, { nargs = 1 })
+local GetUProject = function()
+    local matches = vim.fn.glob("*.uproject", false, true)
+    if #matches == 0 then
+        print("no .uproject file found in current directory")
+    else
+        return matches[1]
+    end
+end, {}
 
-vim.api.nvim_create_user_command('CreateTarget', function(opts)
-    local name = opts.args or "MyProject"
-    local lines = {
-        'using UnrealBuildTool;',
-        'using System.Collections.Generic;',
-        '',
-        'public class '..name..'Target : TargetRules ',
-        '{',
-        '   public '..name..'Target(TargetInfo Target) : base(Target)',
-        '   {',
-        '       Type = TargetType.Game;',
-        '       DefaultBuildSettings = BuildSettingsVersion.V5;',
-        '       IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;',
-        '       ExtraModuleNames.Add("");',
-        '   }',
-        '}',
-    }
-    vim.api.nvim_put(lines, 'l', true, true)
-end, { nargs = 1 })
-
-vim.api.nvim_create_user_command('CreateModuleRules', function(opts)
-    local name = opts.args or "MyProjectCore"
-    local lines = {
-        'using UnrealBuildTool;',
-        '',
-        'public class '..name..' : ModuleRules',
-        '{',
-        '   public '..name..'(ReadOnlyTargetRules Target) : base(Target)',
-        '   {',
-        '       PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;',
-        '',
-        '       PublicDependencyModuleNames.AddRange( new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });',
-        '       PrivateDependencyModuleNames.AddRange( new string[] { } );',
-        '   }',
-        '}',
-    }
-    vim.api.nvim_put(lines, 'l', true, true)
-end, { nargs = 1 })
 
 vim.api.nvim_create_user_command('UECompile', function()
     local cwd = vim.fn.getcwd()
@@ -73,13 +24,40 @@ vim.api.nvim_create_user_command('UECompile', function()
 
     local engine_path = "/home/Agustin/extra/GameDev/Unreal/UnrealEngine-git/Engine/Build/BatchFiles/Linux/Build.sh"
 
-    local cmd = string.format(
+    -- Build command
+    local build_cmd = string.format(
         "%s -mode=GenerateClangDatabase -NoExecCodeGenActions -project=\"%s\" %sEditor Linux Development",
         engine_path, project_path, project_name
     )
 
+    -- Path to generated compile_commands.json
+    local src_compile_db = "/home/Agustin/extra/GameDev/Unreal/UnrealEngine-git/compile_commands.json"
+    -- Destination: project root (same folder as .uproject)
+    local dst_compile_db = string.format("%s/compile_commands.json", cwd)
+
+    -- Chain the build and copy
+    local cmd = string.format("%s && cp %s %s", build_cmd, src_compile_db, dst_compile_db)
+
     vim.cmd("split | terminal " .. cmd)
 end, {
     nargs = 0,
-    desc = 'Generate clang compile_commands.json for the .uproject in current dir',
+    desc = 'Generate clang compile_commands.json for the .uproject in current dir and copy it to project root',
 })
+
+vim.api.nvim_create_user_command('UEditor', function()
+    local cwd = vim.fn.getcwd()
+    local uproject_file = GetUProject()
+    local project_path = cwd .. "/" .. uproject_file
+
+    local engine_path = "/home/Agustin/extra/GameDev/Unreal/UnrealEngine-git/Engine/Binaries/Linux/UnrealEditor"
+    local dxc_path = "/home/Agustin/extra/GameDev/Unreal/UnrealEngine-git/Engine/Extras/ThirdPartyNotUE/DirectXShaderCompiler/bin"
+
+    local cmd = string.format(
+        'env LD_LIBRARY_PATH="%s:$LD_LIBRARY_PATH" %q %q &',
+        dxc_path,
+        engine_path,
+        project_path
+    )
+
+    os.execute(cmd)
+end, {})

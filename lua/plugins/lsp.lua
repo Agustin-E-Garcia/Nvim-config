db6c7ea759045f7494d7bea3f8209952755641f8
Agustin-E-Garcia <Agustinezequielprogrammer@gmail.com>
2025-10-28 14:05:45 -0300

    Added back autoenable lsp support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,3 +3,21 @@ require("mason").setup()
 vim.lsp.config('clangd', {});
 vim.lsp.config('omnisharp', {});
 vim.lsp.config('lua_ls', { settings = { Lua = { diagnostics = { globals = { "vim" } } } } })
+
+local fileType_to_server =
+{
+    c = "clangd",
+    cpp = "clangd",
+    cs = "omnisharp",
+    lua = "lua_ls",
+}
+
+vim.api.nvim_create_autocmd("FileType",
+{
+    callback = function(event)
+            local server = fileType_to_server[event.match]
+            if server then
+                vim.lsp.enable(server)
+            end
+    end,
+})
